0ab407c615c59ba27b2afee495f37d30cd25c8dc
Suejin Kim <kim.suejin.20@gmail.com>
2021-04-27 17:33:45 -0400

    added functions for adding dand viewing departments and roles with inquirer prompts

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const connection = mysql.createConnection({
   });
 
   
-const viewEmployee = () => {
+const viewEmployees = () => {
       console.log("Viewing Employees\n");
       connection.query('SELECT * FROM employee', 
       (err, res) => {
@@ -25,7 +25,29 @@ const viewEmployee = () => {
           startManager();
         })
 }
-    
+
+const viewDepartments = () => {
+    console.log("Viewing Departments\n");
+    connection.query('SELECT * FROM dept', 
+    (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        startManager();
+      })
+
+}
+
+const viewRoles = () => {
+    console.log("Viewing Roles\n");
+    connection.query('SELECT * FROM role', 
+    (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        startManager();
+      })
+
+}
+
 const addEmployee = () => {
     inquirer
         .prompt([
@@ -68,20 +90,106 @@ const addEmployee = () => {
         })
 }
     
-const removeEmployee = () => {
-    console.log("Removing Employee\n");
-    connection.query('DELETE FROM employee WHERE ?',
-    {
-        first_name: 'bob'
-    },
-    (err, res) => {
-        if (err) throw err;
-        console.log('Employee deleted!')
-        //console.table(res);
-        startManager();
-    }
-    )
+const addDepartment = () => {
+    inquirer
+        .prompt([
+            {
+                name: 'dept_name',
+                type: 'input',
+                message: 'What is the first name of the department?'
+            },
+        ])
+        .then((answer) => {
+            connection.query('INSERT INTO dept SET ?',
+            {
+                dept: answer.dept_name,
+            },
+            (err, res) => {
+                if (err) throw err;
+                console.log('Department added!')
+                //console.table(res);
+                startManager();
+            }
+            )
+        })
 }
+
+const addRole = () => {
+    inquirer   
+        .prompt([
+            {
+                name: 'title',
+                type: 'input',
+                message: 'What is the title of this role?'
+            },
+            {
+                name: 'salary',
+                type: 'input',
+                message: 'What is the salary for this role?',
+            },
+            {
+                name: 'dept_id',
+                type: 'input',
+                message: 'What is the department id for this role?',
+            },
+        ])
+        .then((answer) => {
+            connection.query('INSERT INTO role SET ?',
+                {
+                    title: answer.title,
+                    salary: answer.salary,
+                    dept_id: answer.dept_id,
+                },
+                (err, res) => {
+                    if (err) throw err;
+                    console.log('Role added!')
+                    //console.table(res);
+                    startManager();
+                }
+            )
+        })
+}
+
+// const removeEmployee = () => {
+//     connection.query('SELECT * FROM employee', (err, results) => {
+//         if (err) throw err;
+//         inquirer
+//             .prompt([
+//                 {
+//                     name: 'choice',
+//                     type: 'rawlist',
+//                     choices() {
+//                       const choiceArray = [];
+//                       results.forEach(({ first_name, last_name }) => {
+//                         choiceArray.push(first_name, last_name);
+//                       });
+//                       return choiceArray;
+//                     },
+//                     message: 'Please select an employee to remove.',
+//                   },
+//             ])
+//             .then((answer) => {
+//                 let chosenEmployee;
+//                 results.forEach((employee) => {
+//                     if (employee.first_name === answer.choice){
+//                         chosenEmployee = employee
+//                     }
+//                     console.log("Removing Employee\n");
+//                     connection.query('DELETE FROM employee WHERE ?',
+//                     {
+//                         first_name: first_name.choice
+//                     },
+//                     (err, res) => {
+//                         if (err) throw err;
+//                         console.log('Employee deleted!')
+//                         //console.table(res);
+//                         startManager();
+//                     })
+//                 })
+//             })
+//     })
+    
+// }
     
 const updateEmployee = () => {
     console.log("Updating Employee\n")
@@ -109,23 +217,35 @@ const startManager = () => {
             name: 'managerAction',
             type: 'list',
             message: 'What would you like to do?',
-            choices: ['View Employees', 'Add an Employee', 'Update Employee Information', 'Remove an Employee', 'Exit']
+            choices: ['View Employees', 'View Departments', 'View Roles', 'Add an Employee', 'Add a Department', 'Add a Role', 'Update Employee Information', 'Exit']
         })
         .then((answer) => {
             //switch case answer.managerAction
             switch (answer.managerAction) {
                 case 'View Employees':
-                    viewEmployee();
+                    viewEmployees();
+                    break;
+                case 'View Departments':
+                    viewDepartments();
+                    break;
+                case 'View Roles':
+                    viewRoles();
                     break;
                 case 'Add an Employee':
                     addEmployee();
                     break;
+                case 'Add a Department':
+                    addDepartment();
+                    break;
+                case 'Add a Role':
+                    addRole();
+                    break;
                 case 'Update Employee Information':
                     updateEmployee();
                     break;
-                case 'Remove an Employee':
-                    removeEmployee();
-                    break;
+                // case 'Remove an Employee':
+                //     removeEmployee();
+                //     break;
                 default:
                     connection.end();
                     break;
